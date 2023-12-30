6e7fda492a648b61967f162e7ddb7cedc2dc960e
Oliver <oliver.hatina2006@gmail.com>
2023-12-30 17:22:18 +0100

    celsius program branch

--- uloha.js
+++ uloha.js
@@ -1,8 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.vypisatVysledok = void 0;
+function vypisatVysledok(resultName, buttonName) {
+    const currentInput = document.getElementById(buttonName);
+    const resultParagraph = document.getElementById(resultName);
+    if (resultParagraph !== null) {
+        resultParagraph.innerHTML =
+            celsiusToFahrenheit(Number(currentInput.value)).toFixed(2) +
+                " °F";
+    }
+    else {
+        console.error("Did not find element: ", resultName);
+    }
+}
+exports.vypisatVysledok = vypisatVysledok;
 function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
+    return (celsius * 9) / 5 + 32;
 }
 for (let celsius = 0; celsius <= 100; celsius++) {
-    const fahrenheit = (celsius * 9 / 5) + 32;
+    const fahrenheit = celsiusToFahrenheit(celsius);
     console.log(`${celsius} stupňov Celsius = ${fahrenheit.toFixed(2)} stupňov Fahrenheit`);
 }
--- uloha.ts
+++ uloha.ts
@@ -1,8 +1,24 @@
+export function vypisatVysledok(resultName: string, buttonName: string) {
+  const currentInput = document.getElementById(buttonName) as HTMLInputElement;
+
+  const resultParagraph = document.getElementById(resultName);
+
+  if (resultParagraph !== null) {
+    resultParagraph.innerHTML =
+      celsiusToFahrenheit(Number(currentInput.value)).toFixed(2) +
+      " °F";
+  } else {
+    console.error("Did not find element: ", resultName);
+  }
+}
+
 function celsiusToFahrenheit(celsius: number): number {
-  return (celsius * 9 / 5) + 32;
+  return (celsius * 9) / 5 + 32;
 }
 
 for (let celsius = 0; celsius <= 100; celsius++) {
-  const fahrenheit = (celsius * 9 / 5) + 32;
-  console.log(`${celsius} stupňov Celsius = ${fahrenheit.toFixed(2)} stupňov Fahrenheit`);
+  const fahrenheit = celsiusToFahrenheit(celsius);
+  console.log(
+    `${celsius} stupňov Celsius = ${fahrenheit.toFixed(2)} stupňov Fahrenheit`
+  );
 }
