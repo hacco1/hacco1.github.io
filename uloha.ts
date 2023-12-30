abc15eef89f4754f30d392fcb77fd12d2f438997
Oliver <oliver.hatina2006@gmail.com>
2023-12-30 15:17:26 +0100

    wwewe

--- uloha.ts
+++ uloha.ts
@@ -1,31 +1,8 @@
-export function calculateResistance(buttonName: string, resultName: string) {
-  // Získanie hodnoty prúdu zo vstupu
-  const currentInput = document.getElementById(buttonName) as HTMLInputElement;
-  const resultParagraph = document.getElementById(resultName);
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9 / 5) + 32;
+}
 
-  console.log("Value is: ", currentInput.value);
-  console.log("Numbered value is: ", Number(currentInput.value));
-
-  // Predpokladané hodnoty napätia a prúdu
-  const voltage = 220; // v Voltách
-  const current = Number(currentInput.value) / 1000; // prevod z mA na A
-
-  console.log("calculated value: ", current);
-  // Výpočet odporu podľa Ohmovho zákona R = U/I
-  const resistance = voltage / Number(current);
-
-  console.log(
-    "R = " + resistance.toFixed(2) + " ohmov, I = " + currentInput.value + " mA"
-  );
-
-  if (resultParagraph !== null) {
-    resultParagraph.innerHTML =
-      "R = " +
-      resistance.toFixed(2) +
-      " ohmov, I = " +
-      currentInput.value +
-      " mA";
-  } else {
-    console.error("Did not find element: ", resultName);
-  }
+for (let celsius = 0; celsius <= 100; celsius++) {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  console.log(`${celsius} stupňov Celsius = ${fahrenheit.toFixed(2)} stupňov Fahrenheit`);
 }
